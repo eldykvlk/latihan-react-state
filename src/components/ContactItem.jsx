e82e7c31e3187b05220fd637adff2c6095b60f57
eldykoplak <eldykoplak@gmail.com>
2024-11-06 09:42:45 +0000

    add daftar kontak

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import ContactItemImage from "./ContactItemImage.jsx";
 import ContactItemBody from "./ContactItemBody.jsx";
+import DeleteButton from "./DeleteButton.jsx";
 
-function ContactItem({imageUrl, name, tag}){
+function ContactItem({imageUrl, name, tag, id, onDelete}){
     return (
         <div className="contact-item">
             <ContactItemImage imageUrl={imageUrl}/>
             <ContactItemBody name={name} tag={tag}/>
+            <DeleteButton id={id} onDelete={onDelete}/>
         </div>
     );
 }
