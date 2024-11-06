4a6debb6fecb80dd74e6ad01ae61a3a6ebf88fb4
eldykoplak <eldykoplak@gmail.com>
2024-11-06 01:07:21 +0000

    add belajar state

--- src/main.jsx
+++ src/main.jsx
@@ -1,84 +1,87 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-
- 
-function Button({link}) {
-  return <a href={link}>Find out more</a>;
-}
- 
-function CardHeader({image, category}) {
-  return <header>
-    <h4>{category}</h4>
-    <img src={image} alt="gambar" />
-  </header>
-}
- 
-function CardBody({date, title, content, link}) {
-    return <div>
-         <p>{date}</p>
-        <h3>{title}</h3>
-        <p>{content}</p>
-        <Button link={link} />
-    </div>
-  
-}
- 
-function Card({image, category, date, title, content, link}) {
-  return <article>
-    <CardHeader image={image} category={category} />
-    <CardBody date={date} title={title} content={content} link={link} />
-  </article>
-}
- 
-function Header({title, subtitle}) {
-   return <header>
-    <h1>{title}</h1>
-    <p>{subtitle}</p>
-  </header>
-}
- 
-function News() {
-  // data news
-  const someNews = [
-    {
-      title: 'CNN Acuire BEME',
-      date: 'March 20 2022',
-      content: "CNN purchased Casey Neistat's Beme app for $25million.",
-      image: 'https://picsum.photos/600/400',
-      category: 'News',
-      link: '#'
-    },
-    {
-      title: 'React and the WP-API',
-      date: 'March 19 2022',
-      content: 'The first ever decoupled starter theme for React & the WP-API.',
-      image: 'https://picsum.photos/600/400',
-      category: 'News',
-      link: '#'
-    },
-    {
-      title: 'Nomad Lifestyle',
-      date: 'March 19 2022',
-      content: 'Learn our tips and tricks on living a nomadic lifestyle.',
-      image: 'https://picsum.photos/600/400',
-      category: 'Travel',
-      link: '#'
+function CounterDisplay({ count }) {
+    if (count === 0) {
+      return <p>{count}</p>;
     }
-  ];
- 
+   
+    if (count % 5 === 0 && count % 7 === 0) {
+      return <p>FizzBuzz</p>;
+    }
+   
+    if (count % 5 === 0) {
+      return <p>Fizz</p>;
+    }
+   
+    if (count % 7 === 0) {
+      return <p>Buzz</p>;
+    }
+   
+    return <p>{count}</p>;
+  }
 
-  return (
-<div>
-    <Header title="Latest new" subtitle="Covering March & April 2022" />
-    {someNews.map((news)=>(
-        <Card {...news} key={news.title}/>
-    ))}
-
-    
-</div>
-  );
+function IncreaseButton({increase}) {
+    return (
+        <div>
+            <button onClick={increase}>+ increase</button>
+        </div>
+    );
 }
- 
+
+function ResetButton({ reset }) {
+    return (
+      <div>
+        <button onClick={reset}>- reset</button>
+      </div>
+    );
+  }
+
+class CounterApp extends React.Component {
+    constructor(props){
+        super(props);
+
+    //inisialisasi nilai count di dalam state
+    this.state = {
+        count: 0
+    };
+
+     // binding event handler
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+
+}
+
+   
+
+    onIncreaseEventHandler(){
+        this.setState((previousState) => {
+            return {
+                count: previousState.count + 1
+            };
+        });
+    }
+
+    onResetEventHandler(){
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        }
+    );
+    }
+
+
+    render(){
+        return(
+            <div>
+        <IncreaseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+            </div>
+        );
+    }
+}
+
 const root = createRoot(document.getElementById('root'));
-root.render(<News />);
+root.render(<CounterApp />);
